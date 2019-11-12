eed58cc73e539f7b592b2c6cc181528bebcb29f6
Koha Choji <koha.choji@vistair.com>
2019-11-12 08:29:00 +0000

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require("path")
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, 'src', 'app'),
+    entry: path.resolve(__dirname, `src`, `app`),
     output: {
-        path: path.resolve(__dirname,),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/'
     },
@@ -16,7 +16,8 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.(ts|js))x?/,
+            test: /\.(ts|js)x?/,
+            exclude: /node_modules/,
             loader: 'babel-loader'
         }]
     }
