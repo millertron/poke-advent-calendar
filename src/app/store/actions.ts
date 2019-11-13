1a06a12a2b8d13d5acb19ce7668423b5a58c7618
Koha Choji <koha.choji@vistair.com>
2019-11-13 17:56:13 +0000

    Add placeholder action

---
+++ src/app/store/actions.ts
@@ -0,0 +1,6 @@
+export const OPEN_POCKET = `OPEN_POCKET`
+
+export const openPocket = (dayNum: number) => ({
+    type: OPEN_POCKET,
+    dayNum
+})
--- src/app/store/cofigureStore.ts
+++ src/app/store/cofigureStore.ts
@@ -1,10 +1,14 @@
 import { createStore, combineReducers } from 'redux'
 import { defaultState } from '../server/defaultState'
 import { Pocket } from '../types/types'
+import * as actions from "./actions"
 
 export const store = createStore(
     combineReducers({
         pockets(pockets:Pocket[] = defaultState.pockets, action) :Pocket[]{
+            if (action.type === actions.OPEN_POCKET) {
+                console.log("OPEN POCKET NOW FOR DAY:", action.dayNum)
+            }
             return pockets
         }
     })
