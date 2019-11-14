c5e3674e83f6a702c8d985c2e3522c80c845f567
Koha Choji <koha.choji@vistair.com>
2019-11-14 21:42:22 +0000

    Implemented browser router to take in react

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -3,12 +3,15 @@ import { Pocket } from '../types/types'
 import { connect } from 'react-redux'
 import { State } from '../server/defaultState'
 import CalendarPocket from './CalendarPocket'
+import { useParams } from 'react-router'
 
 type Props = {
     pockets: Pocket[]
 }
 
 export const Calendar =({pockets}: Props) => {
+
+    // Can use url parameter: const { urlKey } = useParams(); 
 
     const pocketComponents = pockets.map(pocket => (
         <CalendarPocket key={pocket.dayNum} dayNum={pocket.dayNum} />
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,9 +4,12 @@ import Calendar from './components/Calendar'
 import { defaultState } from './server/defaultState'
 import { Provider } from 'react-redux'
 import { store } from './store/cofigureStore'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Calendar />
-    </Provider>
+    <BrowserRouter>
+        <Provider store={store}>
+            <Route path="/:urlKey" render={()=>(<Calendar />)} />
+        </Provider>
+    </BrowserRouter>
 , document.getElementById("app"))
