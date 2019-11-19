5782843d073c1273f0bffbde7aeff3e546956ef7
Koha Choji <koha.choji@vistair.com>
2019-11-19 12:57:12 +0000

    Added more db operations

--- src/server/repositories/pocketRepository.js
+++ src/server/repositories/pocketRepository.js
@@ -1,5 +1,20 @@
 module.exports = {
-    getPocketsForKey: async (db, key) => {
-        return await db.collection('pockets').find({ key: key }).toArray()
+
+    getPocketsForKey: (db, key) => {
+        return db.collection('pockets').find({ key: key }).toArray()
+    },
+
+    insertPocket: async (db, key, dayNum, pokeId) => {
+        const existing = await db.collection('pockets').find({ key: key, dayNum: dayNum }).toArray()
+        if (existing.length == 0) {
+            db.collection('pockets').insertOne({ key: key, dayNum: dayNum, pokeId: pokeId })
+        }
+    },
+
+    updatePocket: (db, key, dayNum, pokeId) => {
+        db.collection('pockets').updateOne(
+            { key: key, dayNum: dayNum, pokeId: null },
+            { $set: { key: key, dayNum: dayNum, pokeId: pokeId }}
+        )
     }
 }
