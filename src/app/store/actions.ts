a0dafb30a249164d4c95e09dd07dbdea8850daf1
Koha Choji <koha.choji@vistair.com>
2019-11-20 11:03:36 +0000

    Add additional front-end actions

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -1,4 +1,22 @@
-export const OPEN_POCKET = `OPEN_POCKET`
+export const REQUEST_FETCH_POCKETS = 'REQUEST_FETCH_POCKETS'
+export const FETCH_POCKETS = 'FETCH_POCKET'
+export const REQUEST_OPEN_POCKET = 'REQUEST_OPEN_POCKET'
+export const OPEN_POCKET = 'OPEN_POCKET'
+
+export const requestFetchPockets = (dayNum: number) => ({
+    type: REQUEST_FETCH_POCKETS,
+    dayNum
+})
+
+export const fetchPockets = (dayNum: number) => ({
+    type: FETCH_POCKETS,
+    dayNum
+})
+
+export const requestOpenPocket = (dayNum: number) => ({
+    type: REQUEST_OPEN_POCKET,
+    dayNum
+})
 
 export const openPocket = (dayNum: number) => ({
     type: OPEN_POCKET,
