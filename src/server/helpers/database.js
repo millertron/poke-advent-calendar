8ea3c524b482793ef638415f3be6f028d55f6824
Koha Choji <koha.choji@vistair.com>
2019-11-22 13:30:23 +0000

    More debugging

--- src/server/helpers/database.js
+++ src/server/helpers/database.js
@@ -16,7 +16,7 @@ const connect = (done) => {
     }
 
     MongoClient.connect(`mongodb://${authentication}${databaseUrl}`, { useUnifiedTopology: true }, (err, client) => {
-        console.log("Connecting to database:", databaseUrl, databaseName, databaseUser)
+        console.log("Connecting to database:", `mongodb://${databaseUser}:xxxxx@${databaseUrl}`)
         if (err) {
             console.log("Error connecting to MongoDB")
             return done(err)
