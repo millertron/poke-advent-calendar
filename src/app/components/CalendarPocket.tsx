dbcd6c869df79c9aa81cbe6219777819017de4e2
Koha Choji <koha.choji@vistair.com>
2019-11-29 13:58:17 +0000

    Fixed random ordering of calendar pockets

--- src/app/components/CalendarPocket.tsx
+++ src/app/components/CalendarPocket.tsx
@@ -8,6 +8,7 @@ import { Dispatch } from "redux";
 import * as actions from '../store/actions'
 import * as utils from '../helper/utils'
 import './styles/styles.scss'
+import './styles/order.scss'
 
 type Props = {
     urlKey: String,
@@ -15,6 +16,21 @@ type Props = {
     pocket?: Pocket
     openPocketFunction?: Function
 }
+
+/* Arrange in fixed order: 
+2, 8, 24, 11, 13, 
+20, 3, 17, 5, 1, 
+21, 18, 25, 9, 16,
+12, 6, 14, 22, 4,
+15, 7, 19, 23, 10
+*/
+const order = [
+    10, 1, 7, 20, 9,
+    17, 21, 2, 14, 25,
+    4, 16, 5, 18, 20,
+    15, 8, 12, 23, 6,
+    11, 19, 24, 3, 13
+]
 
 const blankPocket:Pocket = {urlKey: "", dayNum: 0, pokeId: null}
 const blankFunction:Function = (num:number) => { console.log("No function defined", num)}
@@ -37,8 +53,9 @@ const createButtonElement = (pocket: Pocket, openPocketFunction: Function) => {
 export const CalendarPocket = ({pocket = blankPocket, openPocketFunction = blankFunction} :Props) => {
 
     const pocketContent = isNull(pocket.pokeId) ? createButtonElement(pocket, openPocketFunction) : createImageHolderElement(pocket)
+    const className = `m-1 order-${order[pocket.dayNum - 1]}`
     return (
-        <div className="m-1">
+        <div className={className}>
             <div className="card pocket-card">
                 {pocketContent}
             </div>
